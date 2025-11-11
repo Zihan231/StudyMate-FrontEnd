1f49ff9a62c07e0f68ea3126930c7765349e4d7b
Zihan231 <Zihanislam231@gmail.com>
2025-11-11 23:25:51 +0600

    Axios Secure Added

---
+++ src/hooks/AxiosSecure/useAxiosSecure.jsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { useContext, useEffect } from "react";
+import AuthContext from "../../contexts/Auth/AuthContext/AuthContext";
+
+const instance = axios.create({
+    baseURL: "http://localhost:3000",
+})
+const useAxiosSecure = () => {
+    const { user } = useContext(AuthContext);
+    useEffect(() => {
+        const requestInterceptor = instance.interceptors.request.use(configs => {
+            configs.headers.authorization = `Bearer ${user?.accessToken}`;
+            return configs;
+        });
+        return () => {
+            instance.interceptors.request.eject(requestInterceptor);
+        }
+    }, [user])
+    return instance;
+}
+export default useAxiosSecure;
+
+
